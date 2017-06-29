215307387f6893da7a7d6a1bb8ef50e5ed761fc7
tonyvergara <antonio@tony-vergara.com>
2017-06-29 09:26:52 -0400

    once again
    the return of the git. once again.

---
+++ index.php
@@ -0,0 +1,150 @@
+<head>
+  <title>Tony Vergara</title>
+  <link rel="stylesheet" type="text/css" href="css/index-body.css">
+  <link rel="stylesheet" type="text/css" href="css/header-footer.css">
+  <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great|Open+Sans:300,700" rel="stylesheet">
+  <link rel="icon" href="img/page-icon.png">
+  <script src="https://use.fontawesome.com/4ad1d83066.js"></script>
+  <!--MAKES MOBILE-RESPONSIVE WORK-->
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <!--//MAKES MOBILE-RESPONSIVE WORK-->
+  <!--ANALYTICS-->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-87301986-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <!--//ANALYTICS-->
+  <!--IS THIS WHAT SEO IS? I CAN'T ACTUALLY TELL-->
+  <meta name="description" content="I use research, strategy, and design to create digital experiences that improve lives.">
+  <meta name="keywords" content="user experience, UX design, research, strategy, wireframing, information architecture, content strategy, digital strategy, experience design, social media, social impact, web design">
+  <link rel="publisher" href="https://www.linkedin.com/in/tonyvergara">
+  <!--//IS THIS WHAT SEO IS? I CAN'T ACTUALLY TELL-->
+</head>
+<body>
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVLZWB8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <div class="wrapper">
+    <!-- THIS IS THE HEADER -->
+    <header> 
+      <a href="http://tony-vergara.com"><span class="title">TONY VERGARA</span></a>
+      <div class="navigation">
+          <a class="navigation-item" href="#summary"><span>About Me</span></a>
+          <a class="navigation-item" href="#work"><span>My Work</span></a>
+          <a class="navigation-item" href="mailto://antonio@tony-vergara.com"><span>Contact</span></a>
+          <a class="navigation-item" href="/v1/index.html"><span>v1</span></a>
+        </div>
+    </header>
+    <!-- THIS IS THE HERO -->
+    <div class="hero">
+      <img src="img/hero-bg.png">
+      <h1>I merge <span class="embiggen">research, strategy,</span> and <span class="embiggen">design</span> to do good work well.</h1>
+    </div>
+    <!-- THIS IS THE ABOUT ME -->
+    <div id="summary">
+        
+        <img src="img/tony-vergara.png">
+        <div class="services">
+          <h3 class="skills title">What I can do:</h3>
+          <ul>
+            <li><i class="fa fa-paint-brush fa-2x"></i>Experience Design</li>
+            <li><i class="fa fa-user-circle-o fa-2x"></i>User Research</li>
+            <li><i class="fa fa-line-chart fa-2x"></i>Digital Strategy</li>
+            <li><i class="fa fa-pencil-square-o fa-2x"></i>Content Strategy</li>
+            <li><i class="fa fa-share-alt fa-2x"></i>Social Media</li>
+            <li><i class="fa fa-code fa-2x"></i>Web Development</li>
+          </ul>
+        </div>
+    </div>
+    <!-- THIS IS THE MY WORK -->
+    <div id="work">
+      <div class="work title">Selected Work</div>
+      <div class="work-contents">
+        <div class="work-tile guardian go-to-case">
+          <a href="case-studies/guardian-case-study.pdf">
+            <div class="work-tile-contents">
+              <img class="work-img" src="img/guardian.png">
+              <div class="case-text guardian">
+                <h3>GUARDIAN BALTIMORE</h3>
+                <p>Building a lasting client relationship with the socially-minded dance company.</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="work-tile comtech coming-soon">
+          <div class="work-tile-contents">
+            <img class="work-img" src="img/comtech.png">
+            <div class="case-text comtech">
+              <h3>COMTECH TELECOMMUNICATIONS</h3>
+              <p>Designing a unified presence for the Telecommunications conglomerate. A story of "too many cooks."</p>
+            </div>
+          </div>
+        </div>
+        <div class="work-tile masschallenge coming-soon">
+          <div class="work-tile-contents">
+            <img class="work-img" src="img/masschallenge.png">
+            <div class="case-text masschallenge">
+              <h3>MASSCHALLENGE</h3>
+              <p>Improving the startup accelerator's navigation with user testing.</p>
+            </div>
+          </div>
+        </div>
+        <div class="work-tile dctv go-to-case">
+          <a href="http://dctv.com">
+            <div class="work-tile-contents">
+              <img class="work-img" src="img/dctv.png">
+              <div class="case-text dctv">
+                <h3>DCTV</h3>
+                <p>Iterating on previous work using web analytics.</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="work-tile swat coming-soon">
+          <div class="work-tile-contents">
+            <img class="work-img" src="img/swat.png">
+            <div class="case-text swat">
+              <h3>THE SWAT APP</h3>
+              <p>Designing an app and user experience strategy for the public safety startup.</p>
+            </div>
+          </div>
+        </div>
+        <div class="work-tile discovereat coming-soon">
+          <div class="work-tile-contents">
+            <img class="work-img" src="img/discovereat.png">
+            <div class="case-text discovereat">
+              <h3>DISCOVEREAT</h3>
+              <p>Discovering and designing an solution for recipe discovery and cooking assistance.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- THIS IS THE CONTACT -->
+    <div class="contact">
+      <!-- <img class="contact-bg" src="img/contact-bg.png">
+       -->
+      <h3>Want to make something good together?</h3>
+      <p class="contact-info">I am available for freelance, contract, and full-time work.</p>
+    
+      <a href="mailto://antonio@tony-vergara.com"><span class="contact-cta">LET'S TALK!</span></a>
+      </div>
+    </div>
+    <!-- THIS IS THE FOOTER -->
+    <footer>
+        <div class="credits">Designed and Built by Tony Vergara</div>
+        <div class="social-icons">
+          <a href="files/Vergara-Resume.pdf"><i class="fa fa-id-card-o fa-2x"></i></a>
+          <a href="mailto://antonio@tony-vergara.com"><i class="fa fa-envelope fa-2x"></i></a>
+          <a href="http://twitter.com/tonyvergara15"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="http://linkedin.com/in/tonyvergara"><i class="fa fa-linkedin fa-2x"></i></a>
+          <a href="https://play.spotify.com/user/antonio.vergara"><i class="fa fa-spotify fa-2x"></i></a>
+          <!-- <a href=""><i class="fa fa-medium fa-2x empty content-circle"></i></a> -->
+        </div>
+      </footer>
+
+  </div>
+</body>
